8ffd2b2b4d0f7aa8aa14108f690d366a80de095c
rubygitflow <nrubygitflow@gmail.com>
2024-03-21 00:09:20 +0600

    add Advantage Shuffle

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # leetcode_javascript
 Solving algorithmic problems on leetcode.com
 
-- [1736. Latest Time by Replacing Hidden Digits](https://leetcode.com/problems/latest-time-by-replacing-hidden-digits/) >>> [latest_time_by_replacing_hidden_digits.js](https://github.com/rubygitflow/leetcode_elixir/blob/master/latest_time_by_replacing_hidden_digits.js)
+- [870. Advantage Shuffle](https://leetcode.com/problems/advantage-shuffle/) >>> [advantage_shuffle.js](https://github.com/rubygitflow/leetcode_javascript/blob/master/advantage_shuffle.js)
+- [1736. Latest Time by Replacing Hidden Digits](https://leetcode.com/problems/latest-time-by-replacing-hidden-digits/) >>> [latest_time_by_replacing_hidden_digits.js](https://github.com/rubygitflow/leetcode_javascript/blob/master/latest_time_by_replacing_hidden_digits.js)
 
 **Tasks: 1  >>> Files: 1**
 
---
+++ advantage_shuffle.js
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/advantage-shuffle/description/
+// 870. Advantage Shuffle
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var advantageCount = function(nums1, nums2) {
+    let ord = Uint16Array.from({length:nums2.length}, (_,i) => i), 
+        output = new Uint32Array(nums2.length),
+        i = 0, j = nums2.length - 1
+    ord.sort((a,b) => nums2[b] - nums2[a])
+    // console.log(ord)
+    nums1.sort((a,b) => b - a)
+    // console.log(nums1)
+    for (let ix of ord) {
+        output[ix] = nums1[i] > nums2[ix] ? nums1[i++] : nums1[j--]
+        // console.log(output)
+    }
+    return output  
+};
+
+console.log(advantageCount([2,7,11,15], [1,10,4,11]));
+// Output: [2,11,7,15]
+console.log(advantageCount([12,24,8,32], [13,25,32,11]));
+// Output: [24,32,8,12]
