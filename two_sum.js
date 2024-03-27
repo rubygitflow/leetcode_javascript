cf607acede5188f02df7bab156a614d2f31f7049
rubygitflow <nrubygitflow@gmail.com>
2024-03-27 12:16:59 +0600

    add Two Sum II

--- two_sum.js
+++ two_sum.js
@@ -28,3 +28,34 @@ console.log(twoSum([3,3], 6));
 // Output: [0,1]
 console.log(twoSum([3,3], 5));
 // Output: []
+
+// ***************************
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+// 167. Two Sum II - Input Array Is Sorted
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSumSorted = function(numbers, target) {
+    let l = 0
+    let r = numbers.length - 1
+    while (l < r) {
+        sum = numbers[l] + numbers[r]
+        if (sum === target) return [l + 1, r + 1];
+        if (sum > target) r--; else l++;
+    }
+    return []
+};
+
+console.log(twoSumSorted([2,7,11,15], 9));
+// Output: [1,2]
+console.log(twoSumSorted([2,3,4], 6));
+// Output: [1,3]
+console.log(twoSumSorted([-1,0], -1));
+// Output: [1,2]
+console.log(twoSumSorted([3,3], 5));
+// Output: []
+console.log(twoSumSorted([2,7,11,15,15,16,17,18,19,20,21,22,23,24,25], 46));
+// Output: [11,15]
