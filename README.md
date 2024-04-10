41c176e2cdf838ab8784994b1299fe8486cbcf6c
rubygitflow <nrubygitflow@gmail.com>
2024-04-10 23:12:16 +0600

    add Search a 2D Matrix

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Solving algorithmic problems on leetcode.com
 - [15. 3Sum](https://leetcode.com/problems/3sum/) >>> [three_sum.js](https://github.com/rubygitflow/leetcode_javascript/blob/master/three_sum.js)
 - [18. 4Sum](https://leetcode.com/problems/4sum/) >>> [four_sum.js](https://github.com/rubygitflow/leetcode_javascript/blob/master/four_sum.js)
 - [49. Group Anagrams](https://leetcode.com/problems/group-anagrams/) >>> [valid_anagram.js](https://github.com/rubygitflow/leetcode_javascript/blob/master/valid_anagram.js)
+- [74. Search a 2D Matrix](https://leetcode.com/problems/search-a-2d-matrix/) >>> [binary_search.js](https://github.com/rubygitflow/leetcode_javascript/blob/master/binary_search.js)
 - [167. Two Sum II - Input Array Is Sorted](https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/) >>> [two_sum.js](https://github.com/rubygitflow/leetcode_javascript/blob/master/two_sum.js)
 - [242. Valid Anagram](https://leetcode.com/problems/valid-anagram/) >>> [valid_anagram.js](https://github.com/rubygitflow/leetcode_javascript/blob/master/valid_anagram.js)
 - [704. Binary Search](https://leetcode.com/problems/binary-search/) >>> [binary_search.js](https://github.com/rubygitflow/leetcode_javascript/blob/master/binary_search.js)
@@ -36,7 +37,7 @@ Solving algorithmic problems on leetcode.com
 ## Addition
 - Matching strings up to letter permutation >>> [matching_strings_up_to_letter_permutation.js](https://github.com/rubygitflow/leetcode_javascript/blob/master/matching_strings_up_to_letter_permutation.js)
 
-**Tasks: 31(1)  >>> Files: 30**
+**Tasks: 32(1)  >>> Files: 30**
 
 ## Install
 ```shell
--- binary_search.js
+++ binary_search.js
@@ -17,7 +17,7 @@ var search = function(nums, target) {
         // right direction
         else l = mid + 1
     }
-    return - 1
+    return -1
 };
 
 console.log('Binary Search');
@@ -25,3 +25,70 @@ console.log(search([-1,0,3,5,9,12], 9));
 // Output: 4
 console.log(search([-1,0,3,5,9,12], 2));
 // Output: -1
+
+// ======================
+// https://leetcode.com/problems/search-a-2d-matrix/description/
+// 74. Search a 2D Matrix
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    let l = 0
+    let r = matrix.length - 1
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2);
+        let last_pos = matrix[mid].length - 1
+        if(matrix[mid][0] == target || matrix[mid].slice(-1) == target) return true
+        if(matrix[mid][0] < target && matrix[mid].slice(-1) > target){
+            let res = search(matrix[mid], target)
+            return res > -1 ? true : false
+        }
+        // left direction
+        if(matrix[mid][0] > target) r = mid - 1
+        // right direction
+        else l = mid + 1
+    }
+    return false
+};
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrixEx = function(matrix, target) {
+    let m = matrix.length;
+    let n = matrix[0].length;
+    let l = 0, r = m * n - 1;
+
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2);
+        let mid_val = matrix[Math.floor(mid / n)][mid % n];
+
+        if (mid_val === target)
+            return true;
+        else if (mid_val < target)
+            l = mid + 1;
+        else
+            r = mid - 1;
+    }
+    return false;
+};
+
+console.log('Search a 2D Matrix');
+console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13));
+// Output: false
+console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 23));
+// Output: true
+console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 34));
+// Output: true
+
+console.log(searchMatrixEx([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13));
+// Output: false
+console.log(searchMatrixEx([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 23));
+// Output: true
+console.log(searchMatrixEx([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 34));
+// Output: true
