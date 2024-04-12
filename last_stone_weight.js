e48fa405352a7e148d31f5ba61059507222046a7
rubygitflow <nrubygitflow@gmail.com>
2024-04-12 13:16:03 +0600

    add Last Stone Weight II

--- last_stone_weight.js
+++ last_stone_weight.js
@@ -29,3 +29,32 @@ console.log(lastStoneWeight([1]));
 // Output: 1
 console.log(lastStoneWeight([31,26,33,21,40]));
 // Output: 9
+
+// ======================
+// https://leetcode.com/problems/last-stone-weight-ii/description/
+// 1049. Last Stone Weight II
+
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeightII = function(stones) {
+    let prev_state = new Set([0]);
+    for(const elem of stones){
+        const state = new Set();
+        for(const prev of prev_state){
+            state.add(prev + elem)
+            state.add(Math.abs(prev - elem))
+        }
+        prev_state = state
+    }
+    return Math.min(...prev_state)
+};
+
+console.log('Last Stone Weight II');
+console.log(lastStoneWeightII([2,7,4,1,8,2]));
+// Output: 0
+console.log(lastStoneWeightII([2,7,4,1,8,1]));
+// Output: 1
+console.log(lastStoneWeightII([31,26,33,21,40]));
+// Output: 5
