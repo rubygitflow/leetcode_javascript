a5348709864944c97b6f0477244c3168df56b26a
rubygitflow <nrubygitflow@gmail.com>
2024-05-12 01:10:36 +0600

    update Minimum Cost to Convert String I

--- minimum_cost_to_convert_string.js
+++ minimum_cost_to_convert_string.js
@@ -38,9 +38,11 @@ var minimumCost = function(source, target, original, changed, cost) {
   return totalCost;
 };
 
-
+console.log("Array's solution")
 console.log(minimumCost("abcd","acbe",["a","b","c","c","e","d"],["b","c","b","e","b","e"],[2,5,5,1,2,20]));
-// Output: 28
+// Output: 28 (5 < 1+2)
+console.log(minimumCost("abcd","acbe",["a","b","c","c","f","e","d"],["b","c","b","f","e","b","e"],[2,5,5,1,2,1,20]));
+// Output: 29 (5 < 1+2+1)
 console.log(minimumCost("aaaa","bbbb",["a","c"],["c","b"],[1,2]));
 // Output: 12
 console.log(minimumCost("aaaa","bbbb",["a","c"],["d","b"],[1,2]));
@@ -51,3 +53,82 @@ console.log(minimumCost("abcd","abce",["a"],["e"],[10000]));
 // Output: -1
 console.log(minimumCost("aaa","aaa",["a"],["e"],[10000]));
 // Output: 0
+
+
+/**
+ * @param {string} source
+ * @param {string} target
+ * @param {character[]} original
+ * @param {character[]} changed
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minimumCostEx = function(source, target, original, changed, cost) {
+  const l = source.length;
+  let actioned_inp = [],
+      actioned_out = [];
+
+  for (let i = 0; i < l; i++) {
+    if (source[i] !== target[i]) {
+      actioned_inp.push(source[i])
+      actioned_out.push(target[i])
+    }
+  }
+  const actioned_inp_set = new Set(actioned_inp),
+        actioned_out_set = new Set(actioned_out),
+        original_set = new Set(original),
+        changed_set = new Set(changed);
+  for (const element of original_set) actioned_inp_set.delete(element);
+  if (actioned_inp_set.size > 0) return -1;
+
+  for (const element of changed_set) actioned_out_set.delete(element);
+  if (actioned_out_set.size > 0) return -1;
+
+  const m = original.length;
+  let graph = {};
+  for (let i = 0; i < m; i++) {
+    graph[original[i]] = graph[original[i]]||{};
+    graph[original[i]][changed[i]] = Math.max(graph[original[i]][changed[i]]||0, cost[i])
+  }
+
+  const union = new Set([...original_set, ...changed_set]);
+
+  // floyd warshall to find minimum cost between each pair of nodes
+  for (const k of union) { // use all paths with node k
+    graph[k] = graph[k]||{};
+    for (const i of original_set) {
+      graph[i] = graph[i]||{};
+      for (const j of changed_set) {
+        graph[i][j] = Math.min(
+          isNaN(graph[i][j]) ? Infinity : graph[i][j],
+          (isNaN(graph[i][k]) ? Infinity : graph[i][k]) +
+            (isNaN(graph[k][j]) ? Infinity : graph[k][j])
+        );
+      }
+    }
+  }
+
+  let totalCost = 0;
+  for (let i = 0; i < l; i++) {
+    if (graph[source[i]][target[i]] === Infinity) return -1;
+    totalCost += graph[source[i]][target[i]]||0;
+  }
+  return totalCost;
+};
+
+console.log("Object's solution")
+console.log(minimumCostEx("abcd","acbe",["a","b","c","c","e","d"],["b","c","b","e","b","e"],[2,5,5,1,2,20]));
+// Output: 28 (5 < 1+2)
+console.log(minimumCostEx("abcd","acbe",["a","b","c","c","f","e","d"],["b","c","b","f","e","b","e"],[2,5,5,1,2,1,20]));
+// Output: 29 (5 < 1+2+1)
+console.log(minimumCostEx("aaaa","bbbb",["a","c"],["c","b"],[1,2]));
+// Output: 12
+console.log(minimumCostEx("aaaa","bbbb",["a","c"],["d","b"],[1,2]));
+// Output: -1
+console.log(minimumCostEx("aaaa","bbbb",["a","c","a","d","e"],["c","b","d","e","b"],[3,2,1,1,1]));
+// Output: 12
+console.log(minimumCostEx("abcd","abce",["a"],["e"],[10000]));
+// Output: -1
+console.log(minimumCostEx("aaa","aaa",["a"],["e"],[10000]));
+// Output: 0
+
