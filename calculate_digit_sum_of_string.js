7570abb3ee2562931e9a35d612d4cd56cecc1995
rubygitflow <nrubygitflow@gmail.com>
2024-06-09 01:48:56 +0600

    add a test case

--- calculate_digit_sum_of_string.js
+++ calculate_digit_sum_of_string.js
@@ -20,9 +20,31 @@ var digitSum = function(s, k) {
   return s;
 };
 
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var digitSumII = function(s, k) {
+  while (s.length > k) {
+    let newString = "";
+    for (let i = 0; i < s.length; i += k)
+      newString += s.substring(i, i + k).split("").reduce((acc, val) => acc + (+val), 0);
+    s = newString;
+  }
+  return s;
+};
+
 console.log(digitSum("11111222229", 3));
 // Output: "132"
 console.log(digitSum("11111222223", 3));
 // Output: "135"
 console.log(digitSum("00000000", 3));
+// Output: "000"
+
+console.log(digitSumII("11111222229", 3));
+// Output: "132"
+console.log(digitSumII("11111222223", 3));
+// Output: "135"
+console.log(digitSumII("00000000", 3));
 // Output: "000"
