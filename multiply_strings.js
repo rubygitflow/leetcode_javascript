4bdd5330d99e033fce8fbef8440a9e5e058b062c
rubygitflow <nrubygitflow@gmail.com>
2024-06-19 07:54:57 +0600

    update Multiply Strings

--- multiply_strings.js
+++ multiply_strings.js
@@ -8,22 +8,18 @@
  * @return {string}
  */
 var multiply = function(num1, num2) {
-    
-};
-var multiply = function(num1, num2) {
-    if (num1 === '0' || num2 === '0') return '0'
-    
-    const m = num1.length, n = num2.length, res = new Array(m+n).fill(0)
-    for (let i=m-1; i>=0; i--) {
-        for (let j=n-1; j>=0; j--) {
-            const p1=i+j, p2=i+j+1
-            let sum = res[p2] + Number(num1[i]) * Number(num2[j])
-            res[p2] = sum%10
-            res[p1] += Math.floor(sum/10)
-        }
+  if (num1 === '0' || num2 === '0') return '0'
+
+  const m = num1.length, n = num2.length, out = new Array(m+n).fill(0)
+  for (let i=m-1; i>=0; i--) {
+    for (let j=n-1; j>=0; j--) {
+      let sum = out[i+j+1] + Number(num1[i]) * Number(num2[j])
+      out[i+j+1] = sum%10
+      out[i+j] += Math.floor(sum/10)
     }
-    if (res[0] === 0) res.shift()
-    return res.join('')
+  }
+  if (out[0] === 0) out.shift()
+  return out.join('')
 }
 
 console.log(multiply("2", "3"))
