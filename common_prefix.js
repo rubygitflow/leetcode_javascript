4ed326bf4d8913b56682d5a5d74e5fa5ceb2917c
rubygitflow <nrubygitflow@gmail.com>
2024-06-20 08:12:59 +0600

    add Find the Length of the Longest Common Prefix

--- common_prefix.js
+++ common_prefix.js
@@ -25,3 +25,44 @@ console.log(longestCommonPrefix(["flow","flow","flow"]))
 // Output: "flow"
 console.log(longestCommonPrefix(["dog"]))
 // Output: ""
+
+
+// ########################
+// https://leetcode.com/problems/find-the-length-of-the-longest-common-prefix/description/
+// 3043. Find the Length of the Longest Common Prefix
+
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @return {number}
+ */
+var longestCommonPrefixAtLeastOne = function(arr1, arr2) {
+  if (arr1.length < 1 || arr2.length < 1) return 0;
+
+  let acc = new Set();
+  for (let x of arr1){
+    while (x > 0){
+      acc.add(x);
+      x = Math.trunc(x / 10);
+    }
+  }
+  let out = 0
+  for(let x of arr2){
+    while (x > 0 || x > 10 ** out){
+      if (acc.has(x)){
+        let str = String(x);
+        out = Math.max(out, str.length);
+      };
+      x = Math.trunc(x / 10);
+    }
+  }
+  return out;
+};
+
+console.log("Find the Length of the Longest Common Prefix")
+console.log(longestCommonPrefixAtLeastOne([1,10,100], [1000, 1]))
+// # Output: 3
+console.log(longestCommonPrefixAtLeastOne([1,2,3], [4,4,4]))
+// # Output: 0
+console.log(longestCommonPrefixAtLeastOne([1,2,3], []))
+// # Output: 0
