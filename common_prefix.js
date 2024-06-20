a4bee7c1a6780da1a787104a0f123bb6d3e8b2be
rubygitflow <nrubygitflow@gmail.com>
2024-06-20 06:35:13 +0600

    refactor Longest Common Prefix

--- common_prefix.js
+++ common_prefix.js
@@ -6,6 +6,8 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
+  if (strs.length < 2) return ""
+
   for (let i=0; i<strs[0].length; i++) {
     for (let j=1; j<strs.length; j++) {
       if (strs[j].length<=i || strs[j][i] != strs[0][i]) return strs[0].slice(0,i);
@@ -21,3 +23,5 @@ console.log(longestCommonPrefix(["dog","racecar","car"]))
 // Output: ""
 console.log(longestCommonPrefix(["flow","flow","flow"]))
 // Output: "flow"
+console.log(longestCommonPrefix(["dog"]))
+// Output: ""
