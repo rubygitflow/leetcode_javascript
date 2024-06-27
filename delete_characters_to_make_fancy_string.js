1e828b610cefe462a206da13235c586910275e03
rubygitflow <nrubygitflow@gmail.com>
2024-06-27 23:11:09 +0600

    add Delete Characters to Make Fancy String

---
+++ delete_characters_to_make_fancy_string.js
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/delete-characters-to-make-fancy-string/description/
+// 1957. Delete Characters to Make Fancy String
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeFancyString = function(s) {
+  return [...s].reduce(([acc, prev_ch, prev2_ch], ch, idx) => {
+    return [
+      ch == prev_ch && ch == prev2_ch ? acc : acc + ch,
+      ch,
+      prev_ch
+    ];
+  }, ['','',''])[0];
+};
+
+console.log(makeFancyString("leeetcode"))
+// Output: "leetcode"
+console.log(makeFancyString("aaabaaaa"))
+// Output: "aabaa"
+console.log(makeFancyString("aab"))
+// Output: "aab"
+
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeFancyStringII = function(s) {
+    let chars = [...s],
+        acc = '';
+    for(let i = 0; i < chars.length; i++){
+        if(s[i] !== s[i - 1] || s[i] !== s[i - 2]) acc += s[i];
+    }
+    return acc;
+};
+
+console.log(makeFancyStringII("leeetcode"))
+// Output: "leetcode"
+console.log(makeFancyStringII("aaabaaaa"))
+// Output: "aabaa"
+console.log(makeFancyStringII("aab"))
+// Output: "aab"
