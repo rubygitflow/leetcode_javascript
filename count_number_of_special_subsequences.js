14187153c1abe2e2f87ccb89846dca64f538385a
rubygitflow <nrubygitflow@gmail.com>
2024-06-28 22:15:25 +0600

    refactor a code with the global constant

--- count_number_of_special_subsequences.js
+++ count_number_of_special_subsequences.js
@@ -2,17 +2,18 @@
 // # 1955. Count Number of Special Subsequences
 // # Explanation: https://algo.monster/liteproblems/1955
 
+const mod = 10**9 + 7
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var countSpecialSubsequences = function(nums) {
-  const mod = Math.pow(10, 9) + 7
   let [total_zeros, total_ones, total_twos] = [0,0,0]
   for (let i = 0; i < nums.length; i++) {
-      if (nums[i] === 0) total_zeros = (total_zeros * 2 + 1) % mod
-      if (nums[i] === 1) total_ones = (total_ones * 2 + total_zeros) % mod
-      if (nums[i] === 2) total_twos = (total_twos * 2 + total_ones) % mod
+    if (nums[i] === 0) total_zeros = (total_zeros * 2 + 1) % mod
+    if (nums[i] === 1) total_ones = (total_ones * 2 + total_zeros) % mod
+    if (nums[i] === 2) total_twos = (total_twos * 2 + total_ones) % mod
   }
   return total_twos 
 };
@@ -33,7 +34,6 @@ console.log(countSpecialSubsequences([0,1,2,0,1,2]))
  * @return {number}
  */
 var countSpecialSubsequencesII = function(nums) {
-  const mod = Math.pow(10, 9) + 7
   return nums.reduce(([total_zeros, total_ones, total_twos], n, idx) => {
     return [
       n === 0 ? total_zeros = (total_zeros * 2 + 1) % mod : total_zeros,
