eb789ed7b78ef7b46979d91f543585ce3dc3a140
rubygitflow <nrubygitflow@gmail.com>
2024-06-29 04:54:52 +0600

    add a test case

--- top_k_frequent_words.js
+++ top_k_frequent_words.js
@@ -6,14 +6,14 @@
  * @param {number} k
  * @return {string[]}
  */
-var topKFrequentWords = function(words, k) {
+var topKFrequentWords = function(words, k = 0) {
   const cnt = new Map();
   for (const word of words) {
       cnt.set(word, (cnt.get(word) || 0) + 1);
   }
   return [...cnt.entries()]
     .sort((a, b) => b[1] !== a[1] ? b[1] - a[1] : a[0].localeCompare(b[0]))
-    .slice(0, k)
+    .slice(0, k===0 ? words.length : Math.abs(k))
     .map(item => item[0]);
 };
 
@@ -23,6 +23,12 @@ console.log(topKFrequentWords(["i","love","leetcode","i","love","coding"], 2));
 // Output: ["i","love"]
 console.log(topKFrequentWords(["the","day","is","sunny","the","the","the","sunny","is","is"], 4));
 // Output: ["the","is","sunny","day"]
+console.log(topKFrequentWords(["the","the","the","day","sunny","sunny","is","is","is"], 2))
+// Output: ["is", "the"]
+console.log(topKFrequentWords(["the","the","the","day","sunny","sunny","is","is","is"], 6))
+// Output: ["is", "the", "sunny", "day"]
+console.log(topKFrequentWords(["that","event","a","was","funny","that","that","that","funny","a","was","was"]));
+// Output: ["that","was","a","funny","event"]
 
 
 // ###################
@@ -42,7 +48,7 @@ var topKFrequentElements = function(nums, k) {
     }
     return [...cnt.entries()]
       .sort((a, b) => b[1] == a[1] ? a[0] - b[0] :  b[1] - a[1])
-      .slice(0, k)
+      .slice(0, k===0 ? nums.length : Math.abs(k))
       .map(item => item[0]);
 };
 
@@ -52,3 +58,15 @@ console.log(topKFrequentElements([1,1,1,2,2,3], 2))
 // Output: [1,2]
 console.log(topKFrequentElements([10,11,13,25,22,30,42,10,], 2))
 // Output: [10, 11]
+console.log(topKFrequentElements([1], 1))
+// Output: [1]
+console.log(topKFrequentElements([10,11,13,25,22,30,42,10], 3))
+// Output: [10, 11, 13]
+console.log(topKFrequentElements([10,11,13,25,22,30,42,10], 0))
+// Output: [10, 11, 13, 22, 25, 30, 42]
+console.log(topKFrequentElements([10,11,13,25,22,30,42,10], -2))
+// Output: [10, 11]
+console.log(topKFrequentElements([1,1,1,2,2,3,0,0,0,5,5,5], 2))
+// Output: [0, 1]
+console.log(topKFrequentElements([1,1,1,2,2,3,5,5,5,0,0,0], 2))
+// Output: [0, 1]
